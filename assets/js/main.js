e688619bdd947921daad56fb5b0b0ce0c66be9b0
kleqing <hotaruu24k@gmail.com>
2025-03-25 22:05:38 +0700

    Update new web

---
+++ assets/js/main.js
@@ -0,0 +1,332 @@
+/**
+* Template Name: MyResume
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Updated: Jun 29 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+  "use strict";
+
+  /**
+   * Header toggle
+   */
+  const headerToggleBtn = document.querySelector('.header-toggle');
+
+  function headerToggle() {
+    document.querySelector('#header').classList.toggle('header-show');
+    headerToggleBtn.classList.toggle('bi-list');
+    headerToggleBtn.classList.toggle('bi-x');
+  }
+  headerToggleBtn.addEventListener('click', headerToggle);
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.header-show')) {
+        headerToggle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentNode.classList.toggle('active');
+      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+      e.stopImmediatePropagation();
+    });
+  });
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector('.scroll-top');
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+  }
+  scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+
+  window.addEventListener('load', toggleScrollTop);
+  document.addEventListener('scroll', toggleScrollTop);
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+
+  /**
+   * Animate the skills items on reveal
+   */
+  let skillsAnimation = document.querySelectorAll('.skills-animation');
+  skillsAnimation.forEach((item) => {
+    new Waypoint({
+      element: item,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = item.querySelectorAll('.progress .progress-bar');
+        progress.forEach(el => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%';
+        });
+      }
+    });
+  });
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init isotope layout and filters
+   */
+  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+    let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+    let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+    let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
+
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+        itemSelector: '.isotope-item',
+        layoutMode: layout,
+        filter: filter,
+        sortBy: sort
+      });
+    });
+
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+      filters.addEventListener('click', function() {
+        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+        this.classList.add('filter-active');
+        initIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        if (typeof aosInit === 'function') {
+          aosInit();
+        }
+      }, false);
+    });
+
+  });
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
+
+  /**
+   * Correct scrolling position upon page load for URLs containing hash links.
+   */
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
+  });
+
+  /**
+   * Navmenu Scrollspy
+   */
+  let navmenulinks = document.querySelectorAll('.navmenu a');
+
+  function navmenuScrollspy() {
+    navmenulinks.forEach(navmenulink => {
+      if (!navmenulink.hash) return;
+      let section = document.querySelector(navmenulink.hash);
+      if (!section) return;
+      let position = window.scrollY + 200;
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+        navmenulink.classList.add('active');
+      } else {
+        navmenulink.classList.remove('active');
+      }
+    })
+  }
+  window.addEventListener('load', navmenuScrollspy);
+  document.addEventListener('scroll', navmenuScrollspy);
+
+  function calculateAge(birthDate) {
+    let today = new Date();
+    let birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    let monthDiff = today.getMonth() - birth.getMonth();
+
+    // Nếu chưa tới sinh nhật trong năm nay, trừ đi 1 tuổi
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+        age--;
+    }
+
+    return age;
+  }
+  document.getElementById("age").innerText = calculateAge("2004-12-28");
+
+  async function fetchGitHubStats() {
+    const username = "kleqing";
+    
+    // API URLs
+    const reposUrl = `https://api.github.com/users/${username}/repos`;
+    const issuesUrl = `https://api.github.com/search/issues?q=author:${username}+type:issue`;
+    const prsUrl = `https://api.github.com/search/issues?q=author:${username}+type:pr`;
+
+    try {
+        // Fetch repositories
+        const reposResponse = await fetch(reposUrl);
+        const repos = await reposResponse.json();
+        const repoCount = repos.length;
+
+        // Calculate total stars
+        const totalStars = repos.reduce((sum, repo) => sum + repo.stargazers_count, 0);
+
+        // Fetch issues & PRs
+        const issuesResponse = await fetch(issuesUrl);
+        const issuesData = await issuesResponse.json();
+        const totalIssues = issuesData.total_count;
+
+        const prsResponse = await fetch(prsUrl);
+        const prsData = await prsResponse.json();
+        const totalPRs = prsData.total_count;
+
+        // Update HTML
+        document.getElementById("repo-count").textContent = repoCount;
+        document.getElementById("star-count").textContent = totalStars;
+        document.getElementById("issue-count").textContent = totalIssues;
+        document.getElementById("pr-count").textContent = totalPRs;
+
+      } catch (error) {
+          console.error("Error fetching GitHub data:", error);
+      }
+  }
+
+  document.addEventListener("DOMContentLoaded", fetchGitHubStats);
+
+  document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll(".progress").forEach(progress => {
+            const valElement = progress.querySelector(".val");
+            const progressBar = progress.querySelector(".progress-bar");
+            const value = valElement.getAttribute("data-value"); // Lấy giá trị từ data-value
+
+            valElement.textContent = value + "%"; // Cập nhật số %
+            progressBar.style.width = value + "%"; // Cập nhật chiều dài thanh progress
+            progressBar.setAttribute("aria-valuenow", value); // Cập nhật aria-valuenow
+        });
+    });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById("theme-toggle");
+    const logo = document.getElementById("dynamic-logo");
+    const body = document.body;
+
+    function updateLogo() {
+      const isDarkMode = document.body.classList.contains("dark-background");
+      logo.src = isDarkMode ? logo.getAttribute("data-src-dark") : logo.getAttribute("data-src-light");
+    }
+
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-background");
+        themeToggle.classList.replace("bi-moon-stars", "bi-brightness-high");
+    } else {
+        body.classList.add("light-background");
+        themeToggle.classList.replace("bi-brightness-high", "bi-moon-stars");
+    }
+
+    updateLogo();
+
+    themeToggle.addEventListener("click", function () {
+        if (body.classList.contains("dark-background")) {
+            body.classList.remove("dark-background");
+            body.classList.add("light-background");
+            localStorage.setItem("theme", "light");
+            themeToggle.classList.replace("bi-brightness-high", "bi-moon-stars");
+        } else {
+            body.classList.remove("light-background");
+            body.classList.add("dark-background");
+            localStorage.setItem("theme", "dark");
+            themeToggle.classList.replace("bi-moon-stars", "bi-brightness-high");
+            
+        }
+      updateLogo();
+    });
+  });
+
+})();
