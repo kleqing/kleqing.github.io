3a4b6919844742735d27c2b3639fe73cb12f5052
kleqing <nhanpk28122004@gmail.com>
2026-01-07 00:41:28 +0700

    [Update] Change source path (test deployment)

--- src/routes/nijigen/+page.js
+++ src/routes/nijigen/+page.js
@@ -1,9 +1,7 @@
 import { getPosts } from '$lib/js/posts.js';
 
-export const prerender = true;
-
 export async function load() {
-	const modules = import.meta.glob('/src/content/nijigen/*/*.md');
+	const modules = import.meta.glob('/src/content/nijigen/**/*.md');
 	let posts = await getPosts(modules);
 
 	// //* Sort by name
